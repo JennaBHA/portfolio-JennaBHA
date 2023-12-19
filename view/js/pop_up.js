2d61fe985805c458f8c415bcaa7ee71d73b3c109
JennaBHA <benhadj.jenna@gmail.com>
2023-12-19 14:21:51 +0100

    finission du portfolio

--- view/js/pop_up.js
+++ view/js/pop_up.js
@@ -1,36 +1,51 @@
 // Fonctions pour le premier popup
 function showPopup() {
-    document.getElementById("popup").style.display = "flex";
+  document.getElementById("popup").style.display = "flex";
+  document.body.classList.add("popup-active");
+  document.querySelector(".situation-content").style.zIndex = "0";
 }
 
 function closePopup() {
-    document.getElementById("popup").style.display = "none";
+  document.getElementById("popup").style.display = "none";
+  document.body.classList.remove("popup-active");
+  document.querySelector(".situation-content").style.zIndex = "1";
 }
 
 // Fonctions pour le deuxième popup
 function showPopup1() {
-    document.getElementById("popup1").style.display = "flex";
+  document.getElementById("popup1").style.display = "flex";
+  document.body.classList.add("popup-active");
+  document.querySelector(".situation-content").style.zIndex = "0";
 }
 
 function closePopup1() {
-    document.getElementById("popup1").style.display = "none";
+  document.getElementById("popup1").style.display = "none";
+  document.body.classList.remove("popup-active");
+  document.querySelector(".situation-content").style.zIndex = "1";
 }
 
 // Fonctions pour le troixieme popup
 function showPopup2() {
-    document.getElementById("popup2").style.display = "flex";
+  document.getElementById("popup2").style.display = "flex";
+  document.body.classList.add("popup-active");
+  document.querySelector(".situation-content").style.zIndex = "0";
 }
 
 function closePopup2() {
-    document.getElementById("popup2").style.display = "none";
+  document.getElementById("popup2").style.display = "none";
+  document.body.classList.remove("popup-active");
+  document.querySelector(".situation-content").style.zIndex = "1";
 }
 
 // Fonctions pour le troixieme popup
 function showPopup3() {
-    document.getElementById("popup3").style.display = "flex";
+  document.getElementById("popup3").style.display = "flex";
+  document.body.classList.add("popup-active");
+  document.querySelector(".situation-content").style.zIndex = "0";
 }
 
 function closePopup3() {
-    document.getElementById("popup3").style.display = "none";
+  document.getElementById("popup3").style.display = "none";
+  document.body.classList.remove("popup-active");
+  document.querySelector(".situation-content").style.zIndex = "1";
 }
-
