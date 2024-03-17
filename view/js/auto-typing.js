c695bb932e939e6cce0d26c5eb46b3ed1f4e5f79
JennaBHA <benhadj.jenna@gmail.com>
2024-03-17 14:46:18 +0100

    amélioration

--- view/js/auto-typing.js
+++ view/js/auto-typing.js
@@ -1,7 +1,19 @@
-var typed = new Typed(".auto-type",{
-    strings : ["Developer", "Developer", "Programmer", "Designer"],
-    typeSpeed : 150,
-    backspeed : 50, 
-    looped : true
-})
+    function initTyped() {
+        var typed = new Typed(".auto-type", {
+            strings: ["Developer", "Developer", "Programmer", "Designer"],
+            typeSpeed: 150,
+            backSpeed: 50,
+            loop: false,
+            onComplete: function () {
+                setTimeout(function () {
+                    typed.reset(); // Réinitialiser le texte après un court délai
+                    typed.start(); // Redémarrer l'auto typing
+                }, 1000); // Délai en millisecondes avant de réinitialiser
+            }
+        });
+    }
 
+    // Initialiser Typed lors du chargement de la page
+    document.addEventListener("DOMContentLoaded", function () {
+        initTyped();
+    });
--- view/js/navbar.js
+++ view/js/navbar.js
@@ -1,4 +1,41 @@
-/* navbar.js */
+ /* navbar.js */
+ const navbarLinks = document.querySelectorAll('.navbar a');
+
+ // Parcourez chaque lien et ajoutez un gestionnaire d'événements pour le clic
+ navbarLinks.forEach(function(link) {
+   link.addEventListener('click', function(event) {
+     // Retirez la classe 'active' de tous les autres liens
+     navbarLinks.forEach(function(link) {
+       link.classList.remove('active');
+     });
+
+     // Ajoutez la classe 'active' au lien qui a été cliqué
+     this.classList.add('active');
+   });
+ });
+
+ // Récupérer le titre de la page actuelle
+ const pageTitle = document.title;
+ const currentPageLink = document.querySelector(`.navbar a[href="${pageTitle.toLowerCase()}.html"]`);
+
+ // Si le lien correspondant à la page actuelle existe, ajoutez la classe 'active' pour indiquer le focus
+ if (currentPageLink) {
+   currentPageLink.classList.add('active');
+ }
+
+ // Ajoute un gestionnaire d'événements pour rétablir automatiquement le focus sur le dernier lien sélectionné
+ document.addEventListener('focusout', function(event) {
+   // Vérifiez si l'élément qui a perdu le focus est un lien de la navbar
+   if (!event.relatedTarget || !event.relatedTarget.classList.contains('navbar')) {
+     // Déplacez le focus vers le lien de la page actuelle
+     const currentPageLink = document.querySelector('.navbar .active');
+     if (currentPageLink) {
+       currentPageLink.focus();
+     }
+   }
+ });
+
+ /* navbar.js */
 const header = document.querySelector('.header');
 
 // Fonction pour changer la classe en fonction de la position de défilement
@@ -15,3 +52,6 @@ window.addEventListener('scroll', toggleNavbarColor);
 
 // Appel initial pour gérer l'état initial de la navbar
 toggleNavbarColor();
+
+
+
